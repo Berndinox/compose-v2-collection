559495cb190723ffb4517430a3dbab39ca209806
Berndinox <berndinox@gmail.com>
2016-12-19 09:39:57 +0100

    Update start.sh
    Make OVH-Cloud Storage optional

--- start.sh
+++ start.sh
@@ -9,11 +9,23 @@ then
     export PASSWORD=$input1
 fi
 
-if [ -z "$OVH_PASSWORD" ]
-then
-    read -p  "Enter OVH_PASSWORD:" input2
-    export OVH_PASSWORD=$input2
-fi
+# IF OVH Storage, uncomment this!
+#
+#if [ -z "$OVH_PASSWORD" ]
+#then
+#    read -p  "Enter OVH_PASSWORD:" input2
+#    export OVH_PASSWORD=$input2
+#fi
+#if [ -z "$OVH_USER" ]
+#then
+#    read -p  "Enter OVH_USER:" input3
+#    export OVH_USER=$input3
+#fi
+#if [ -z "$OVH_TENANT" ]
+#then
+#    read -p  "Enter OVH_TENANT:" input4
+#    export OVH_TENANT=$input4
+#fi
 
 
 if [[ "$(docker network ls | grep proxy 2> /dev/null)" == "" ]]; then
